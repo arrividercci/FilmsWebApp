6e4ba02daf8cf2d772cdaa5612897a1559c60400
Влад Кучинський <vchelovek0@gmail.com>
2023-02-20 19:50:53 +0200

    Added connection string

--- FilmsWebApp/Models/Program.cs
+++ FilmsWebApp/Models/Program.cs
@@ -1,8 +1,11 @@
+using FilmsWebApp;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<MovieDbContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
